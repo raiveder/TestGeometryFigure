5fe36f983db9f4f87173c560c0ba3bd8315eb2b2
Дмитрий Сергеев <kolovrat-86@yandex.ru>
2023-04-02 17:42:13 +0300

    Готово

---
+++ TestShool/Figures.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace TestShool
+{
+    delegate Geometry CreateFiguresDelegate(int x, bool isUp); // Чтобы можно было сделать массив из функций для создания фигур.
+
+    internal class Figures
+    {
+        private Random s_random = new Random();
+        private int _heightContainer;
+        private int _widthContainer;
+        private int _sizeFigures;
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса Figures с заданными параметрами
+        /// </summary>
+        /// <param name="sizeFigures">размер фигур (длина и ширина)</param>
+        /// <param name="heightContainer">высота контейнера</param>
+        /// <param name="widthContainer">ширина контейнера</param>
+        public Figures(int sizeFigures, int heightContainer, int widthContainer)
+        {
+            _heightContainer = heightContainer;
+            _widthContainer = widthContainer;
+            _sizeFigures = sizeFigures;
+        }
+
+        /// <summary>
+        /// Создаёт треугольник с заданными параметрами
+        /// </summary>
+        /// <param name="x">позиция по оси Х крайней левой точки фигуры</param>
+        /// <param name="isUp">true - фигура располагается в верхней половине контейнера, false - в нижней</param>
+        /// <returns>Треугольник с заданными начальной позицией и вертикальным положением</returns>
+        public Geometry CreateTriangle(int x, bool isUp)
+        {
+            int y = GetCoordinateY(isUp);
+
+            return GetGeometryFromPoints(new PointCollection()
+            {
+                new Point(x, y),
+                new Point(x + _sizeFigures / 2, y - _sizeFigures),
+                new Point(x + _sizeFigures, y)
+            });
+        }
+
+        /// <summary>
+        /// Создаёт квадрат с заданными параметрами
+        /// </summary>
+        /// <param name="x">позиция по оси Х крайней левой точки фигуры</param>
+        /// <param name="isUp">true - фигура располагается в верхней половине контейнера, false - в нижней</param>
+        /// <returns>Квадрат с заданными начальной позицией и вертикальным положением</returns>
+        public Geometry CreateSquare(int x, bool isUp)
+        {
+            int y = GetCoordinateY(isUp);
+
+            return GetGeometryFromPoints(new PointCollection()
+            {
+                new Point(x, y),
+                new Point(x, y - _sizeFigures),
+                new Point(x + _sizeFigures, y - _sizeFigures),
+                new Point(x + _sizeFigures, y)
+            });
+        }
+
+        /// <summary>
+        /// Создаёт круг с заданными параметрами
+        /// </summary>
+        /// <param name="x">позиция по оси Х крайней левой точки фигуры</param>
+        /// <param name="isUp">true - фигура располагается в верхней половине контейнера, false - в нижней</param>
+        /// <returns>Круг с заданными начальной позицией и вертикальным положением</returns>
+        public Geometry CreateEllipse(int x, bool isUp)
+        {
+            int y = GetCoordinateY(isUp);
+
+            return new EllipseGeometry
+            (
+                new Point
+                (
+                    x + _sizeFigures / 2,
+                    y - _sizeFigures / 2
+                ),
+                _sizeFigures / 2,
+                _sizeFigures / 2
+            );
+        }
+
+        /// <summary>
+        /// Создаёт ромб с заданными параметрами
+        /// </summary>
+        /// <param name="x">позиция по оси Х крайней левой точки фигуры</param>
+        /// <param name="isUp">true - фигура располагается в верхней половине контейнера, false - в нижней</param>
+        /// <returns>Ромб с заданными начальной позицией и вертикальным положением</returns>
+        public Geometry CreateRhomb(int x, bool isUp)
+        {
+            int y = GetCoordinateY(isUp);
+
+            return GetGeometryFromPoints(new PointCollection()
+            {
+                new Point(x, y + _sizeFigures / 2),
+                new Point(x + _sizeFigures / 2, y),
+                new Point(x + _sizeFigures, y + _sizeFigures / 2),
+                new Point(x + _sizeFigures / 2, y + _sizeFigures)
+            });
+        }
+
+        /// <summary>
+        /// Создаёт пламя с заданными параметрами
+        /// </summary>
+        /// <param name="x">позиция по оси Х крайней левой точки фигуры</param>
+        /// <param name="isUp">true - фигура располагается в верхней половине контейнера, false - в нижней</param>
+        /// <returns>Пламя с заданными начальной позицией и вертикальным положением</returns>
+        public Geometry CreateFlame(int x, bool isUp)
+        {
+            int y = GetCoordinateY(isUp);
+
+            return GetGeometryFromPoints(new PointCollection()
+            {
+                new Point(x + _sizeFigures / 6, y),
+                new Point(x, y - _sizeFigures / 4),
+                new Point(x + _sizeFigures / 2, y - _sizeFigures),
+                new Point(x + _sizeFigures, y - _sizeFigures / 4),
+                new Point(x + 5* _sizeFigures / 6, y)
+            });
+        }
+
+        /// <summary>
+        /// Создаёт пятиугольник с заданными параметрами
+        /// </summary>
+        /// <param name="x">позиция по оси Х крайней левой точки фигуры</param>
+        /// <param name="isUp">true - фигура располагается в верхней половине контейнера, false - в нижней</param>
+        /// <returns>Пятиугольник с заданными начальной позицией и вертикальным положением</returns>
+        public Geometry CreatePentagon(int x, bool isUp)
+        {
+            int y = GetCoordinateY(isUp);
+
+            return GetGeometryFromPoints(new PointCollection()
+            {
+                new Point(x + _sizeFigures / 6, y),
+                new Point(x, y - _sizeFigures + _sizeFigures / 4),
+                new Point(x + _sizeFigures / 2, y - _sizeFigures),
+                new Point(x + _sizeFigures, y - _sizeFigures + _sizeFigures / 4),
+                new Point(x + 5 * _sizeFigures / 6, y)
+            });
+        }
+
+        /// <summary>
+        /// Создаёт звезду с заданными параметрами
+        /// </summary>
+        /// <param name="x">позиция по оси Х крайней левой точки фигуры</param>
+        /// <param name="isUp">true - фигура располагается в верхней половине контейнера, false - в нижней</param>
+        /// <returns>Звезда с заданными начальной позицией и вертикальным положением</returns>
+        public Geometry CreateStar(int x, bool isUp)
+        {
+            int y = GetCoordinateY(isUp);
+
+            int count = 5;
+            int outerRadius = _sizeFigures / 2;
+            int innerRadius = outerRadius / 2;
+            double alpha = 0;
+
+            PointCollection points = new PointCollection() { new Point(x, y) };
+
+            for (int i = 0; i < 2 * count + 1; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    points.Add(new Point(x + innerRadius * Math.Cos(alpha), y + innerRadius * Math.Sin(alpha)));
+                }
+                else
+                {
+                    points.Add(new Point(x + outerRadius * Math.Cos(alpha), y + outerRadius * Math.Sin(alpha)));
+                }
+                alpha += Math.PI / count;
+            }
+
+            return GetGeometryFromPoints(points);
+        }
+
+        /// <summary>
+        /// Генерирует случайную координату Y для фигуры в зависимости от её вертикального расположения
+        /// </summary>
+        /// <param name="IsUp">true - фигура располагается в верхней половине контейнера, false - в нижней</param>
+        /// <returns>Координата Y крайней нижней точки фигуры</returns>
+        public int GetCoordinateY(bool IsUp)
+        {
+            if (IsUp)
+            {
+                return s_random.Next(_sizeFigures + 1, _heightContainer / 2);
+            }
+            else
+            {
+                return s_random.Next(_heightContainer / 2 + _sizeFigures + 1, _heightContainer);
+            }
+        }
+
+        /// <summary>
+        /// Создаёт фигуру на основании её точек
+        /// </summary>
+        /// <param name="points">коллекция точек для отрисовки фигуры</param>
+        /// <returns>Фигура, созданная по заданным точкам</returns>
+        public static Geometry GetGeometryFromPoints(PointCollection points)
+        {
+            Geometry geometry = new StreamGeometry();
+
+            using (StreamGeometryContext ctx = ((StreamGeometry)geometry).Open())
+            {
+                ctx.BeginFigure(points[0], true, true);
+                ctx.PolyLineTo(points, true, false);
+            }
+
+            return geometry;
+        }
+
+        /// <summary>
+        /// Вычисляет отступы между фигурами (с учётом размеров самой фигуры)
+        /// </summary>
+        /// <param name="countFigures">количество фигур</param>
+        /// <returns></returns>
+        public int GetOffset(int countFigures)
+        {
+            int offset = (_widthContainer - countFigures * _sizeFigures) / (countFigures / 2);
+
+            if (countFigures % 2 == 0)
+            {
+                offset--;
+            }
+
+            return offset + _sizeFigures;
+        }
+    }
+}
--- TestShool/MainWindow.xaml.cs
+++ TestShool/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
@@ -11,43 +11,64 @@ namespace TestShool
     /// </summary>
     public partial class MainWindow : Window
     {
-        Random Rnd = new Random();
-
         public MainWindow()
         {
             InitializeComponent();
 
-            CreateFigure();
+            ShowFigures(CreateFigures(), cnvQuestionFirst);
         }
 
-        private void CreateFigure()
+        /// <summary>
+        /// Создаёт фигуры.
+        /// </summary>
+        /// <returns>Коллекция фигур</returns>
+        private List<Geometry> CreateFigures()
         {
-            int size = 40;
-            int widthContainer = 150;
-            int heightContainer = 150;
+            Figures figures = new Figures(40, 200, 400); // Размер фигур, высота и ширина контейнера.
 
-            List<Geometry> figures;
+            List<CreateFiguresDelegate> createFiguresMethods = new List<CreateFiguresDelegate> // Сюда добавляем фигуры, которые нам надо.
+            {                                                                                  // Эту процедуру надо будет сделать через рандом,
+                figures.CreateTriangle,                                                        // чтобы они спавнились в случайном порядке (без повторений).
+                figures.CreateSquare,                                                          // Но это уде ответственность человека, использующего данный функционал.
+                figures.CreateEllipse,                                                         // Для примера использования я сделал хардкодом, как и некоторые другие вещи.
+                figures.CreateRhomb,
+                figures.CreateFlame,
+                figures.CreatePentagon,
+                figures.CreateStar
+            };
 
-            while (CheckIntersections())
+            List<Geometry> listFigures = new List<Geometry>();
+            int offset = figures.GetOffset(createFiguresMethods.Count); // Отступы между фигурами (высчитывается автоматически на основании ширины контейнера,
+            int x = 0;                                                  // при желании можно вручную указать.
+
+            for (int i = 0; i < createFiguresMethods.Count; i++)
             {
-                figures = new List<Geometry>
+                if (i % 2 == 0)
                 {
-                    CreateTriangle(size, widthContainer, heightContainer),
-                    CreateSquare(size, widthContainer, heightContainer),
-                    CreateEllipse(size, widthContainer, heightContainer),
-                    CreateRhomb(size, widthContainer, heightContainer),
-                    CreateFlame(size, widthContainer, heightContainer),
-                    CreatePentagon(size, widthContainer, heightContainer),
-                    CreateStar(size, widthContainer, heightContainer)
-                };
-
+                    listFigures.Add(createFiguresMethods[i](x, true)); // Положение фигуры по вертикали вверху.
+                }
+                else
+                {
+                    listFigures.Add(createFiguresMethods[i](x, false)); // Положение фигуры по вертикали внизу.
+                    x += offset;
+                }
             }
 
-            cnvAnswerFirst.Children.Clear();
+            return listFigures;
+        }
+
+        /// <summary>
+        /// Добавляет созданные фигуры в элемент управления "Canvas"
+        /// </summary>
+        /// <param name="figures">коллекция фигур</param>
+        /// <param name="canvas">элемент управления "Canvas"</param>
+        private void ShowFigures(List<Geometry> figures, Canvas canvas)
+        {
+            canvas.Children.Clear();
 
             foreach (Geometry item in figures)
             {
-                cnvAnswerFirst.Children.Add(new Path()
+                canvas.Children.Add(new Path()
                 {
                     StrokeThickness = 3,
                     Stroke = (Brush)new BrushConverter().ConvertFrom("#F14C18"),
@@ -55,334 +76,5 @@ namespace TestShool
                 });
             }
         }
-
-        private bool CheckIntersections()
-        {
-            return true;
-        }
-
-        private Geometry CreateTriangle(int size, int widthContainer, int heightContainer)
-        {
-            Point pointStart = new Point(Rnd.Next(1, widthContainer - size), Rnd.Next(size, heightContainer - 1));
-            Point[] points = new Point[] {
-                new Point(pointStart.X + size / 2, pointStart.Y - size),
-                new Point(pointStart.X + size, pointStart.Y)
-            };
-
-            Geometry g = new StreamGeometry();
-            using (StreamGeometryContext ctx = ((StreamGeometry)g).Open())
-            {
-                ctx.BeginFigure(pointStart, true, true);
-                ctx.PolyLineTo(points, true, false);
-            }
-
-            return g;
-        }
-
-        private Geometry CreateSquare(int size, int widthContainer, int heightContainer)
-        {
-            Geometry g = new RectangleGeometry(new Rect
-                (
-                new Point(Rnd.Next(widthContainer), Rnd.Next(heightContainer - size)),
-                new Size(size, size)
-                ));
-
-            return g;
-        }
-
-        private Geometry CreateEllipse(int size, int widthContainer, int heightContainer)
-        {
-            Geometry g = new EllipseGeometry
-                (
-                new Point(
-                    Rnd.Next(size + 1, widthContainer - size),
-                    Rnd.Next(size + 1, heightContainer - size)
-                    ),
-                size / 2,
-                size / 2
-                );
-
-            return g;
-        }
-
-        private Geometry CreateRhomb(int size, int widthContainer, int heightContainer)
-        {
-            Point pointStart = new Point(Rnd.Next(size / 2 + 1, widthContainer - size / 2), Rnd.Next(size + 1, heightContainer - 1));
-            Point[] points = new Point[]
-            {
-                new Point(pointStart.X - size / 2, pointStart.Y - size / 2),
-                new Point(pointStart.X, pointStart.Y - size),
-                new Point(pointStart.X + size / 2, pointStart.Y - size / 2)
-            };
-
-            Geometry g = new StreamGeometry();
-            using (StreamGeometryContext ctx = ((StreamGeometry)g).Open())
-            {
-                ctx.BeginFigure(pointStart, true, true);
-                ctx.PolyLineTo(points, true, false);
-            }
-
-            return g;
-        }
-
-        private Geometry CreateFlame(int size, int widthContainer, int heightContainer)
-        {
-            Point pointStart = new Point(Rnd.Next(1, widthContainer - size), Rnd.Next(size - size / 4 + 1, heightContainer - size / 4));
-            Point[] points = new Point[]
-            {
-                new Point(pointStart.X + size / 2, pointStart.Y - size + size / 4),
-                new Point(pointStart.X + size, pointStart.Y),
-                new Point(pointStart.X + 5 * size / 6, pointStart.Y + size / 4),
-                new Point(pointStart.X + size / 6, pointStart.Y + size / 4)
-            };
-
-            Geometry g = new StreamGeometry();
-            using (StreamGeometryContext ctx = ((StreamGeometry)g).Open())
-            {
-                ctx.BeginFigure(pointStart, true, true);
-                ctx.PolyLineTo(points, true, false);
-            }
-
-            return g;
-        }
-
-        private Geometry CreatePentagon(int size, int widthContainer, int heightContainer)
-        {
-            Point pointStart = new Point(Rnd.Next(1, widthContainer - size), Rnd.Next(size - size / 4 + 1, heightContainer - size / 4));
-            Point[] points = new Point[]
-            {
-                new Point(pointStart.X + size / 2, pointStart.Y - size / 4),
-                new Point(pointStart.X + size, pointStart.Y),
-                new Point(pointStart.X + size - size / 6, pointStart.Y + size - size / 4),
-                new Point(pointStart.X + size / 6, pointStart.Y + size - size / 4)
-            };
-
-            Geometry g = new StreamGeometry();
-            using (StreamGeometryContext ctx = ((StreamGeometry)g).Open())
-            {
-                ctx.BeginFigure(pointStart, true, true);
-                ctx.PolyLineTo(points, true, false);
-            }
-
-            return g;
-        }
-
-        private Geometry CreateStar(int size, int widthContainer, int heightContainer)
-        {
-            int count = 5;
-            int outerRadius = size / 2;
-            int innerRadius = outerRadius / 2;
-            double alpha = 0;
-            PointCollection points = new PointCollection()
-            {
-                new Point(Rnd.Next(size / 2 + 1, widthContainer - size / 2),
-                Rnd.Next(size + 1, heightContainer - 1))
-            };
-
-            for (int i = 0; i < 2 * count + 1; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    points.Add(new Point(points[0].X + innerRadius * Math.Cos(alpha), points[0].Y + innerRadius * Math.Sin(alpha)));
-                }
-                else
-                {
-                    points.Add(new Point(points[0].X + outerRadius * Math.Cos(alpha), points[0].Y + outerRadius * Math.Sin(alpha)));
-                }
-                alpha += Math.PI / count;
-            }
-
-            Geometry g = new StreamGeometry();
-            using (StreamGeometryContext ctx = ((StreamGeometry)g).Open())
-            {
-                ctx.BeginFigure(points[0], true, true);
-                ctx.PolyLineTo(points, true, false);
-            }
-
-            return g;
-        }
-
-        //private int GetCoordinateY(int size, int sizeContainer, bool IsUp)
-        //{
-        //    if (IsUp)
-        //    {
-        //        return Rnd.Next(size + 1, sizeContainer / 2);
-        //    }
-        //    else
-        //    {
-        //        return Rnd.Next(sizeContainer / 2 + size + 1, sizeContainer);
-        //    }
-        //}
-
-        //private static bool HasIntersection(Geometry g1, Geometry g2)
-        //{
-        //    return g1.FillContainsWithDetail(g2) != IntersectionDetail.Empty;
-        //}
-
-        //private static Transform GetFullTransform(UIElement e)
-        //{
-        //    var transforms = new TransformGroup();
-
-        //    if (e.RenderTransform != null)
-        //        transforms.Children.Add(e.RenderTransform);
-
-        //    var xTranslate = (double)e.GetValue(Canvas.LeftProperty);
-        //    if (double.IsNaN(xTranslate))
-        //        xTranslate = 0D;
-
-        //    var yTranslate = (double)e.GetValue(Canvas.TopProperty);
-        //    if (double.IsNaN(yTranslate))
-        //        yTranslate = 0D;
-
-        //    var translateTransform = new TranslateTransform(xTranslate, yTranslate);
-        //    transforms.Children.Add(translateTransform);
-
-        //    return transforms;
-        //}
-
-        //private Geometry RenderedIntersect(Visual ctx, Polygon s)
-        //{
-        //    var t = s.TransformToAncestor(ctx) as Transform;
-        //    var g = s.RenderedGeometry;
-        //    g.Transform = t;
-        //    return g;
-        //}
-
-
-
-
-        //private Polygon CreateSquare(ref int x, int size, int sizeContainer, bool IsUp)
-        //{
-        //    int y = GetCoordinateY(size, sizeContainer, IsUp);
-
-        //    return new Polygon()
-        //    {
-        //        Points = new PointCollection()
-        //        {
-        //            new Point(x, y),
-        //            new Point(x, y -= size),
-        //            new Point(x += size, y),
-        //            new Point(x, y + size),
-        //        },
-        //        StrokeThickness = 3,
-        //        Stroke = (Brush)new BrushConverter().ConvertFrom("#F14C18")
-        //    };
-        //}
-
-        //private Ellipse CreateEllipse(ref int x, int size, int sizeContainer, bool IsUp)
-        //{
-        //    int marginY;
-        //    if (IsUp)
-        //    {
-        //        marginY = Rnd.Next(sizeContainer / 2 - size);
-        //    }
-        //    else
-        //    {
-        //        marginY = Rnd.Next(sizeContainer / 2, sizeContainer - size);
-        //    }
-
-        //    x += size;
-
-        //    Geometry g = new EllipseGeometry(new Point(100, 100), 50, 50);
-
-        //    return new Ellipse()
-        //    {
-        //        Width = size,
-        //        Height = size,
-        //        StrokeThickness = 3,
-        //        Stroke = (Brush)new BrushConverter().ConvertFrom("#F14C18"),
-        //        Margin = new Thickness(x - size, marginY, 0, 0)
-        //    };
-        //}
-
-        //private Polygon CreateRhomb(ref int x, int size, int sizeContainer, bool IsUp)
-        //{
-        //    int y = GetCoordinateY(size, sizeContainer, IsUp);
-
-        //    return new Polygon()
-        //    {
-        //        Points = new PointCollection()
-        //        {
-        //            new Point(x + size / 2, y),
-        //            new Point(x, y -= size / 2),
-        //            new Point(x += size / 2, y - size / 2),
-        //            new Point(x += size / 2, y),
-        //        },
-        //        StrokeThickness = 3,
-        //        Stroke = (Brush)new BrushConverter().ConvertFrom("#F14C18")
-        //    };
-        //}
-
-        //private Polygon CreateFlame(ref int x, int size, int sizeContainer, bool IsUp)
-        //{
-        //    int y = GetCoordinateY(size, sizeContainer, IsUp);
-
-        //    return new Polygon()
-        //    {
-        //        Points = new PointCollection()
-        //        {
-        //            new Point(x + size / 6, y),
-        //            new Point(x, y - size / 4),
-        //            new Point(x += size / 2, y - size),
-        //            new Point(x += size / 2, y - size / 4),
-        //            new Point(x - size / 6, y)
-        //        },
-        //        StrokeThickness = 3,
-        //        Stroke = (Brush)new BrushConverter().ConvertFrom("#F14C18")
-        //    };
-        //}
-
-        //private Polygon CreatePentagon(ref int x, int size, int sizeContainer, bool IsUp)
-        //{
-        //    int y = GetCoordinateY(size, sizeContainer, IsUp);
-
-        //    return new Polygon()
-        //    {
-        //        Points = new PointCollection()
-        //        {
-        //            new Point(x + size / 6, y),
-        //            new Point(x, y - size + size / 4),
-        //            new Point(x += size / 2, y - size),
-        //            new Point(x += size / 2, y - size + size / 4),
-        //            new Point(x - size / 6, y)
-        //        },
-        //        StrokeThickness = 3,
-        //        Stroke = (Brush)new BrushConverter().ConvertFrom("#F14C18")
-        //    };
-        //}
-
-        //private Polygon CreateStar(ref int x, int size, int sizeContainer, bool IsUp)
-        //{
-        //    int y = GetCoordinateY(size, sizeContainer, IsUp);
-        //    x += size / 2;
-
-        //    int count = 5;
-        //    int outerRadius = size / 2;
-        //    int innerRadius = outerRadius / 2;
-        //    double alpha = 0;
-        //    PointCollection points = new PointCollection();
-
-        //    for (int i = 0; i < 2 * count + 1; i++)
-        //    {
-        //        if (i % 2 == 0)
-        //        {
-        //            points.Add(new Point(x + innerRadius * Math.Cos(alpha), y + innerRadius * Math.Sin(alpha)));
-        //        }
-        //        else
-        //        {
-        //            points.Add(new Point(x + outerRadius * Math.Cos(alpha), y + outerRadius * Math.Sin(alpha)));
-        //        }
-        //        alpha += Math.PI / count;
-        //    }
-
-        //    x += size / 2;
-
-        //    return new Polygon()
-        //    {
-        //        Points = points,
-        //        StrokeThickness = 3,
-        //        Stroke = (Brush)new BrushConverter().ConvertFrom("#F14C18")
-        //    };
-        //}
     }
 }
